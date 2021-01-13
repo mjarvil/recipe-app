7ebb462fc9d331ccf0d0787221bd9779cb73db90
Mark Avila <markjustineavila@gmail.com>
2021-01-13 10:01:23 +0800

    minor fixes

--- src/components/Recipes/Recipe/Recipe.js
+++ src/components/Recipes/Recipe/Recipe.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { assignSelectedRecipe } from '../../../actions';
-// import './Recipe.css';
 
 import { Card, Image } from 'antd';
 
